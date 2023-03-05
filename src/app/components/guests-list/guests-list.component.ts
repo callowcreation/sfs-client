cef8ef2db5df19ba3451bb64afe82bb3a97e1c44
caLLowCreation <callowcreation@gmail.com>
2023-03-04 22:04:55 -0800

    Transaction framework

--- src/app/components/guests-list/guests-list.component.ts
+++ src/app/components/guests-list/guests-list.component.ts
@@ -5,6 +5,7 @@ import { BackendApiService } from 'src/app/services/backend-api.service';
 import { SettingsService } from 'src/app/services/settings.service';
 import { TwitchLibService } from 'src/app/services/twitch-lib.service';
 import { TwitchUsersService } from 'src/app/services/twitch-users.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-guests-list',
@@ -15,31 +16,66 @@ export class GuestsListComponent {
 
     guests: any[] = [];
 
-    constructor(twitchLib: TwitchLibService, private twitchUsers: TwitchUsersService, backendApi: BackendApiService, public settings: SettingsService) {
-        
+    constructor(private twitchLib: TwitchLibService, private twitchUsers: TwitchUsersService, backendApi: BackendApiService, public settings: SettingsService) {
+
         twitchLib.authorized$.subscribe((auth: TwitchAuth) => {
             backendApi.get<any>(`/shoutouts/${75987197}`).subscribe(data => {
-                // console.log(data)
+                console.log(data)
                 // const guests: any[] = [
                 //     {
+                //         legacy: true,
+                //         broadcaster_id: '75987197',
                 //         streamer_id: 'callowcreation',
                 //         poster_id: 'naivebot',
                 //         timestamp: Date.now()
                 //     }
                 // ];
 
-                const smallData = data.map((x: any) => ([`login=${x.streamer_id}`, `login=${x.poster_id}`])).flat();//.splice(0, 10);
+                const smallData = data.map(this.guestIds).flat();//.splice(0, 10);
                 // smallData.length = 3;
                 twitchUsers.append(smallData) //['login=callowcreation', 'login=naivebot']
                     .then(() => {
                         this.guests = this.removeDuplicates(data);
                         // console.log(this.guests)
+                        // twitchLib.send(data);
                     });
             });
         });
     }
 
-    removeDuplicates(arr: any[]) {
+    actionClick(guest: Guest, action: 'move-up' | 'pin-to-top') {
+        // this.twitchLib.send({ guest, action });
+        const bits = this.twitchLib.bits;
+
+        bits.onTransactionCancelled(() => {
+            console.log('Transaction move up was cancelled');
+        });
+
+        bits.onTransactionComplete((transaction: any) => {
+            this.twitchLib.send({
+                transaction,
+                environment: {
+                    production: environment.production,
+                    version: environment.version,
+                    cycle: environment.cycle,
+                }
+            });
+
+            // this.ebsService.postMoveUp(this.auth.token, transactionData).subscribe();
+        });
+
+        // const sku = this.getSkuTierFromLocalStorage();
+        bits.useBits('move-up-10');
+
+        console.log(`${guest.streamer_id} ${action} callback usebits`);
+    }
+
+    private guestIds(guest: Guest) {
+        const key = guest.legacy === true ? 'login' : 'id';
+        return ([`${key}=${guest.streamer_id}`, `${key}=${guest.poster_id}`]);
+    }
+
+    private removeDuplicates(arr: any[]) {
         return arr.filter((value: Guest, index, self) =>
             index === self.findIndex((t: Guest) => {
                 const u1 = this.twitchUsers.user(t.streamer_id);
--- src/app/services/twitch-lib.service.ts
+++ src/app/services/twitch-lib.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { TwitchAuth } from '../interfaces/twitch-auth';
 import { WindowRef } from '../window-ref';
 
@@ -16,10 +17,30 @@ export class TwitchLibService {
         return this.winRef.nativeWindow.Twitch.ext;
     }
 
+    public get bits(): any {
+        return this.ext.bits;
+    }
+
     constructor(private winRef: WindowRef, private zone: NgZone) {
+        
+        this.ext.bits.setUserLoopBack = true; // used to stop bit transaction from using my bits when testing
+		this.ext.bits.showBitsBalance();
+
         this.ext.onAuthorized((auth: any) => {
             this.auth = auth;
             this.zone.run(() => this.authorized$.next(auth));
         });
+
+        this.ext.listen('broadcast', (target: string, contentType: string, message: string) => {
+            const pub_sub_message = JSON.parse(message);
+            console.log({msg: 'Received broadcast message', target, contentType, pub_sub_message, cycle: `isProduction: ${environment.production}`});
+            this.zone.run(() => {
+
+            });
+        });
+    }
+    
+    send(message: any) {
+        this.ext.send('broadcast', 'application/json', JSON.stringify(message));
     }
 }
